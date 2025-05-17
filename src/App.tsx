7743bd9180852442a9963217b85b07c97edc7b48
Sérgio Adriano <sergioadrianomc@gmail.com>
2025-05-17 14:20:24 -0300

    Ajustes finais na configuração do ambiente de desenvolvimento. Add fontes, e já acesso ao localhost

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App font-roboto">
+      <h1>Bem-vindo ao Azulia</h1>
+      <p>Site de Agentes de IA e Automação</p>
+    </div>
+  )
+}
+
+export default App 
